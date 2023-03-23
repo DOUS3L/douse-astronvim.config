6e06f619b0bdf9c7c436166dd0c37d45c4dfa0bc
DOUS3L <aldous.elpizo@ajaib.co.id>
2023-03-23 22:50:10 +0700

    upd8

--- plugins/colorschemes.lua
+++ plugins/colorschemes.lua
@@ -3,18 +3,6 @@ return {
     "RRethy/nvim-base16",
   },
   {
-    "neanias/everforest-nvim",
-    version = false,
-    lazy = false,
-    priority = 1000, -- make sure to load this before all the other start plugins
-    -- Optional; default configuration will be used if setup isn't called.
-    config = function()
-      require("everforest").setup({
-        background = "hard",
-      })
-    end,
-  },
-  {
     "catppuccin/nvim",
   }
 }
--- plugins/venv-selector.lua
+++
@@ -1,7 +0,0 @@
-return {
-  {
-    "linux-cultist/venv-selector.nvim",
-    opts = {},
-    keys = { { "<leader>lv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
-  },
-}
