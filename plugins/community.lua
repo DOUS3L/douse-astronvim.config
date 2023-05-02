4db5528f6eedd62afc8327ccc8adabb357c974df
DOUS3L <aldous.elpizo@ajaib.co.id>
2023-05-02 16:38:10 +0700

    upd8

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,5 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.project.nvim-spectre" },
-  { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.motion.leap-nvim" },
 }
